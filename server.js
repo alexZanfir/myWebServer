998d859b5c1cea9691929a8a8b16e47b781f994b
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-18 19:20:47 +0200

    test8

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express =  require('express');
+var weather = require('./weather.js');
+var location = require('./location.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 app.get('/about', function(req, res){
